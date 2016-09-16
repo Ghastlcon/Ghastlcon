ef2bbbe713f5f98b4eee30976e387e40e514ebfc
Ghastlcon <2638167136@qq.com>
2016-09-16 21:52:59 +0800

    Create 1258.cpp

---
+++ 1258.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int n, a[50], u[50];
+
+void dfs(int s)
+{
+	int i;
+	
+	if(s >= n)
+	{
+		for(i = 0;i < n;i ++)
+			cout << a[i] << ' ';
+		cout << endl;
+		return;
+	}
+	for(i = 1;i <= n;i ++)
+		if(!u[i])
+		{
+			a[s] = i;
+			u[i] = 1;
+			dfs(s + 1);
+			u[i] = 0;
+		}
+	
+	return;
+}
+
+int main()
+{
+	freopen("1258.in" , "r", stdin);
+	freopen("1258.out", "w", stdout);
+	
+	cin >> n;
+	dfs(0);
+	
+	return 0;
+}
